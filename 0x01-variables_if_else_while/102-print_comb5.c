b11e35187ecb826b68082a4d582fa497e3b09b80
Ernest Chukwunta <ernestchukwunta@yahoo.com>
2023-02-17 15:37:58 -0800

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,8 +17,7 @@ int main(void)
 			putchar((x % 10) + '0');
 			putchar(' ');
 			putchar((y / 10) + '0');
-			putchar((y % 10) + '0');
-			
+			putchar((y % 10) + '0');		
 			if (x == 98 && y == 99)
 				continue;
 			putchar(',');
