cd19fac66f5d6d70a917829fefa676386748f7fb
Ernest Chukwunta <ernestchukwunta@yahoo.com>
2023-02-17 14:38:03 -0800

    Task 8. Hexademical

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char letter;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchat(letter);
+	putchar('\n');
+	return (0);
