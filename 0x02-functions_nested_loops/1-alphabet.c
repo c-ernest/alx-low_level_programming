5607432c994546243fe54078824e9917ed43c35b
Ernest Chukwunta <ernestchukwunta@yahoo.com>
2023-02-22 00:08:46 -0800

    task 1. yet another fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,13 @@
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
-	char i;
+	int i;
 
 	for (i = 'a'; i <= 'z'; i++)
+	{
 		_putchar(i);
+	}
 	_putchar('\n');
-	return (0);
-}
+}	
