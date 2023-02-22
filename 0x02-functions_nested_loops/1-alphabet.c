8aee8a7b1f5f20705abdad153fd122951867267f
Ernest Chukwunta <ernestchukwunta@yahoo.com>
2023-02-22 00:14:59 -0800

    task 1. yet another fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - function that prints lowercase alphabets followed by new line
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
