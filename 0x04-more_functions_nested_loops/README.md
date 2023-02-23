b211758dc67c2f8b6d65ebed65ca634a38479c51
Ernest Chukwunta <ernestchukwunta@yahoo.com>
2023-02-23 07:28:42 -0800

    Task 0. A function that checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+A directory of more functions and nested loops in c programming.
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Author: Ernest Chukwunta 
+ * Desc: Header file containing prototypes for all functions
+ * 	used in the 0x04-more_functions_nested_loops directory
+ */
+
+int _isupper(int c);
+
+
+#endif
