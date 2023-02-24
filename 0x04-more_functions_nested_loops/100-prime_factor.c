e775842d6865d2adb13a21f2978e5fc0aa62e5dd
Ernest Chukwunta <ernestchukwunta@yahoo.com>
2023-02-23 23:42:00 -0800

    Task 11. Largest Prime factor of an integer

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,36 +1,37 @@
 #include <stdio.h>
+#include <math.h>
 
 /**
  * main - find and print the largest prime factor of 612852475143
  *
  * Return: Always 0
  */
-int main(void)
+int getMaxPF (int n)
 {
-	long int n;
+	int i, max = -1;
 
-	n = 612852475143;
-
-	long int div = 2, ans = 0, maxFac;
-
-	while (n != 0)
+	while (n % 2 == 0)
 	{
-		if (n % div != 0)
+		max = 2;
+		n = n/2; //reduce n by half
+	}
+	for (i = 3; i <= sqrt(n); i = i+2)
+	{
+		//odd numbers
+		while (n % i == 0)
 		{
-			div = div + 1;
-		}
-		else
-		{
-			maxFac = n;
-			n = n /div;
-			if (n == 1)
-			{
-				printf("%d", maxFac);
-				ans = 1;
-				break;
-			}
+			max = i;
+			n = n/i;
 		}
 	}
-	printf('\n');
-	return (0);
+	if (n > 2)
+	{
+		max = n;
+	}
+	return max;
+}
+main ()
+{
+	n = 612852475143
+	printf("%d", getMaxPF(n));
 }
