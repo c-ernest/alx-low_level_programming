11d6fdcaa5537935060a745ba9234e4aa94000da
Ernest Chukwunta <ernestchukwunta@yahoo.com>
2023-02-27 13:38:00 -0800

    Task 0. A pointer to an int as parameter and updates the value it points to 98.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - Function to take a pointer to an int and
- * 		and updates the value it points to: 98.
+ * and updates the value it points to: 98.
  * @n: The pointer to an int
  */
 
