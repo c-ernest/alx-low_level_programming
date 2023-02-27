d1d39efb5a429f648a08814f8b18908b805673ac
Ernest Chukwunta <ernestchukwunta@yahoo.com>
2023-02-27 15:10:14 -0800

    Task 5. Function to reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - Function to reverse a  string
+ * @s: reversing string
+ *
+ */
+
+void rev_string(char *s)
+{
+	int len = 0, index = 0;
+	char str;
+
+	while (s[index++])
+	{
+		len++;
+	}
+
+	for (index = len - 1; index >= len / 2; index--)
+	{
+		str = s[index];
+		s[index] = s[len - index - 1];
+		s[len - index - 1] = str;
+	}
+}
