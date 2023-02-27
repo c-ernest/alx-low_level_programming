e20b8969fcbd326ad2f479c85c8b66150f8fa515
Ernest Chukwunta <ernestchukwunta@yahoo.com>
2023-02-27 13:35:54 -0800

    Task 0. A pointer to an int as parameter and updates the value it points to 98.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Function to take a pointer to an int and
+ * 		and updates the value it points to: 98.
+ * @n: The pointer to an int
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Author: Ernest Chukwunta
+ * Desc: Header file containing prototypes for all functions
+ * used in the 0x005-pointers_array_strings
+ */
+
+int _putchar(char c);
+void reset_to_98(int *n);
+
+
+#endif
