e3e389f2d9303b40a2fc44c1a55e33b6ae453ab1
Ernest Chukwunta <ernestchukwunta@yahoo.com>
2023-02-27 16:01:32 -0800

    Task 8. copying

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * strcpy - copies a string pointed to by @src including
+ * null byte, to a buffer pointed to @dest.
+ * @dest:  a buffer to string to
+ * @src: the source of string to copy.
+ * 
+ * Return: a pointer to the destination string @dest.
+ */
+
+char *_strcpy(char *dest, const char *src)
+{
+	int index = 0;
+
+	while (src[index])
+	{
+		des[index] = src[index];
+		index++;
+	}
+
+	retrun (dest);
+}
