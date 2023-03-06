6d56a0606736fd75cfd22200e6da22e7259e92c5
Ernest Chukwunta <ernestchukwunta@yahoo.com>
2023-03-06 14:08:06 -0800

    Task 0. Function that fills memory with a constant byte

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,2 +1,13 @@
+#ifdef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Author: Ernest Chukwunta
+ * Desc: Header file containing prototypes for all functions used
+ * in the 0x07-pointers_arrays_strings directory
+ */
+
+
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
